cbfdad42a4866e8aae724b445e62311671957234
Miquel Ruiz <self@miquelruiz.net>
2025-04-08 22:08:44 -0400

    Ditch websocket reader
    The websocket reader was used to mitigate a bug in UniFi Access
    where the webhook would only push censored names ("M***z").
    
    This has been fixed and we can use the webhook alone.

--- cmd/start.go
+++ cmd/start.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"context"
-	"crypto/tls"
 	"errors"
 	"log"
 	"net/http"
@@ -10,17 +8,11 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
-	"time"
 
 	"github.com/fatcatfablab/doorbot2/httphandlers"
 	"github.com/fatcatfablab/doorbot2/sender"
 	"github.com/fatcatfablab/doorbot2/types"
-	"github.com/fatcatfablab/doorbot2/wsreader"
 	"github.com/spf13/cobra"
-)
-
-const (
-	wsRetry = 5 * time.Second
 )
 
 var (
@@ -31,10 +23,7 @@ var (
 	key          string
 	slackToken   string
 	slackChannel string
-	doordUrl     string
 	tz           string
-	wsAddr       string
-	wsToken      string
 
 	startCmd = &cobra.Command{
 		Use:   "start",
@@ -51,10 +40,7 @@ func init() {
 	pf.StringVar(&key, "key", "certs/key.pem", "Path to the private key")
 	pf.StringVar(&slackToken, "slackToken", os.Getenv("DOORBOT2_SLACK_TOKEN"), "Slack token")
 	pf.StringVar(&slackChannel, "slackChannel", os.Getenv("DOORBOT2_SLACK_CHANNEL"), "Slack channel")
-	pf.StringVar(&doordUrl, "doordUrl", os.Getenv("DOORBOT2_DOORD_URL"), "Doord integration url")
 	pf.StringVar(&tz, "timezone", "America/New_York", "Time zone")
-	pf.StringVar(&wsAddr, "wsAddr", os.Getenv("DOORBOT2_WS_URL"), "http service address")
-	pf.StringVar(&wsToken, "token", os.Getenv("DOORBOT2_WS_TOKEN"), "auth token")
 
 	rootCmd.AddCommand(startCmd)
 }
@@ -62,15 +48,11 @@ func init() {
 func start(cmd *cobra.Command, args []string) {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, syscall.SIGINT)
-	ctx, cancel := context.WithCancel(context.Background())
 	wg := sync.WaitGroup{}
 
 	slack := sender.NewSlack(slackChannel, slackToken)
 	httpServer := initHttpServer(slack)
 	go startHttpServer(&wg, httpServer)
-	wg.Add(1)
-
-	go initWsReader(ctx, &wg, slack)
 	wg.Add(1)
 
 	s := <-done
@@ -80,7 +62,6 @@ func start(cmd *cobra.Command, args []string) {
 		log.Printf("error closing http server: %s", err)
 	}
 
-	cancel()
 	wg.Wait()
 }
 
@@ -110,45 +91,4 @@ func startHttpServer(wg *sync.WaitGroup, s *http.Server) {
 	} else {
 		log.Print("http server closed gracefully")
 	}
-}
-
-// This function doesn't return until the context is cancelled. If it fails to
-// connect to the websocket, or if the connection dies, it retries after 10s.
-func initWsReader(ctx context.Context, wg *sync.WaitGroup, slack types.Sender) {
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
-	}
-
-	for {
-		if errors.Is(ctx.Err(), context.Canceled) {
-			break
-		}
-
-		wr, err := wsreader.New(
-			wsAddr,
-			wsToken,
-			httpClient,
-			accessDb,
-			slack,
-		)
-		if err != nil {
-			log.Printf("error initializing websocket reader: %s", err)
-			time.Sleep(wsRetry)
-			continue
-		}
-
-		if err := wr.StartReader(ctx); err != nil {
-			log.Printf("websocket error: %s", err)
-			time.Sleep(wsRetry)
-			continue
-		}
-
-		log.Print("websocket closed gracefully")
-	}
-
-	wg.Done()
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/fatcatfablab/doorbot2
 go 1.23.4
 
 require (
-	github.com/coder/websocket v1.8.12
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/slack-go/slack v0.15.0
 	github.com/spf13/cobra v1.8.1
